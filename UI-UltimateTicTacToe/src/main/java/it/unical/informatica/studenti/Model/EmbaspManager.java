c903cd3e5ec9cead4682a1116f71a95c4dd5b719
MonettaChristianFrancesco <mntcrs02l08g975q@studenti.unical.it>
2024-05-09 14:14:58 +0200

    adding Embasp as library

--- UI-UltimateTicTacToe/src/main/java/it/unical/informatica/studenti/Model/EmbaspManager.java
+++ UI-UltimateTicTacToe/src/main/java/it/unical/informatica/studenti/Model/EmbaspManager.java
@@ -5,7 +5,6 @@ import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.base.OptionDescriptor;
 import it.unical.mat.embasp.base.Output;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
-import it.unical.mat.embasp.languages.asp.ASPMapper;
 import it.unical.mat.embasp.languages.asp.AnswerSet;
 import it.unical.mat.embasp.languages.asp.AnswerSets;
 import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
