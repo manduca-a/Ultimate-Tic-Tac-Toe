efae6900a18c958b04defeb6b5d2d3540c9de1ee
MonettaChristianFrancesco <mntcrs02l08g975q@studenti.unical.it>
2024-05-11 11:17:05 +0200

    odio github e i conflitti

--- UI-UltimateTicTacToe/src/main/java/it/unical/informatica/studenti/Main.java
+++ UI-UltimateTicTacToe/src/main/java/it/unical/informatica/studenti/Main.java
@@ -7,6 +7,5 @@ public class Main
     public static void main( String[] args )
     {
         GameView.launch();
-        //WorldGame.getInstance().avviaPlayervsCPU(); //da togliere quando si potrà selezionare dall UI
     }
 }
